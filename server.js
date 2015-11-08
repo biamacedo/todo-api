756d7a28487ac4876b6a861ea427f7ee5a1beb5d
bianet13 <bia.macedo13@gmail.com>
2015-11-08 20:20:57 -0200

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -97,34 +97,32 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
-
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
+	var attributes = {};
 
 	// Validate Completed
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		// Provided attribute but it's not a boolean
-		return res.status(404).send();
-	}
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	} 
 
 	// Validate Description
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description
-	} else if (body.hasOwnProperty('description')) {
-		// Provided attribute but it's not a boolean
-		return res.status(404).send();
-	}
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description
+	} 
 
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	db.todo.findById(todoId).then(function(todo){
+ 		if (todo){
+ 			todo.update(attributes).then(function(todo){
+				res.json(todo.toJSON());
+			}, function(e){
+				res.status(400).send(e);
+			});
+ 		} else {
+			return res.status(404).send();
+ 		}
+	}, function(e){
+		res.status(500).send(e);
+	})
 });
 
 db.sequelize.sync().then(function(){
