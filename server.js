315f2305b3b8cb8818fbeceb32db2af94e775d45
bianet13 <bia.macedo13@gmail.com>
2015-11-08 19:27:37 -0200

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -42,25 +42,26 @@ app.get('/todos', function(req, res) {
 
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
-	// Only works if ids are on the same position on the array
-	//res.json(todos[req.params.id]);
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
-
-	// var matchedTodo;
-	// todos.forEach(function(todo){
-	// 	if (todo.id === todoId){
-	// 		matchedTodo = todo;
-	// 	}
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
 	// });
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
+
+	db.todo.findById(todoId).then(function(todo){
+		if (!!todo){
+			res.json(todo);
+		} else {
+			res.status(404).send('Todo with id requested not found.');
+		}
+	}, function(e){
+		res.status(404).send(e);
+	})
 
 });
 
@@ -73,20 +74,6 @@ app.post('/todos', function(req, res) {
 	}, function(e){
 		res.status(400).json(e);
 	})
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(404).send();
-	// }
-
-	// body.description = body.description.trim();
-	// body.id = todoNextId;
-	// todoNextId++;
-
-	// todos.push(body);
-
-	// console.log('description: ' + body.description);
-
-	// res.json(body);
 });
 
 // DELETE /todos/:id
