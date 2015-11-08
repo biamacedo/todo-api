346672407ddefaaed42c170bd0ae1186f7541f7f
bianet13 <bia.macedo13@gmail.com>
2015-11-08 19:12:11 -0200

    Add todo model and POST /todos

--- server_old.js
+++ server_old.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-var db = require('./db.js');
 
 var app = express();
 
@@ -68,25 +67,19 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function(todo){
-		res.json(todo.toJSON());
-	}, function(e){
-		res.status(400).json(e);
-	})
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(404).send();
+	}
 
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(404).send();
-	// }
+	body.description = body.description.trim();
+	body.id = todoNextId;
+	todoNextId++;
 
-	// body.description = body.description.trim();
-	// body.id = todoNextId;
-	// todoNextId++;
+	todos.push(body);
 
-	// todos.push(body);
+	console.log('description: ' + body.description);
 
-	// console.log('description: ' + body.description);
-
-	// res.json(body);
+	res.json(body);
 });
 
 // DELETE /todos/:id
@@ -139,9 +132,6 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-db.sequelize.sync().then(function(){
-	app.listen(PORT, function() {
-		console.log('Express listening on port ' + PORT + '!');
-	});
+app.listen(PORT, function() {
+	console.log('Express listening on port ' + PORT + '!');
 });
-
